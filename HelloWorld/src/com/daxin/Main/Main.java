f94f784a8103e6b2b4aedbbc452643135c1614b8
Dax1n <568275045@qq.com>
2016-09-22 18:28:31 +0800

    添加使用步骤和打印HelloWorld

--- HelloWorld/src/com/daxin/Main/Main.java
+++ HelloWorld/src/com/daxin/Main/Main.java
@@ -1,9 +1,19 @@
 package com.daxin.Main;
 
+/**
+ * github 使用
+ * 1： 在github上创建仓库
+ * 2：仓库克隆到本地文件夹中
+ * 3：创建项目，右键team之后share project 选择git。
+ * 4：仓库路径选择步骤2中克隆下来的仓库。
+ * @author 青春常驻Dax1n
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
 
 	}
 
